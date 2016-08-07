688bf6c3b86971d360cec946d9d1cfd07b3d791b
Rahul Gannerlla <rahulgannerlla@gmail.com>
2016-08-07 01:18:36 -0400

    express, http get mongoDB <-> nodeJS

--- practice/Week 1/npm/app.js
+++ practice/Week 1/npm/app.js
@@ -1,3 +1,14 @@
-var express = require('express'),
-    cons = require('consolidate'),
-    mongodb = require('mongodb');
+var MongoClient = require('mongodb').MongoClient,
+	assert = require('assert');
+
+MongoClient.connect('mongodb://localhost:27017/video', function(err, db){
+	assert.equal(null, err);
+	console.log("successful connected to server");
+
+	db.collection('movies').find({}).toArray(function(err, docs){
+		docs.forEach(function(doc){
+			console.log(doc.title);
+		});
+		db.close();
+	});
+});
